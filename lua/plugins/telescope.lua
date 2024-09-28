3159cc9dd53d4a38243d0728deccb89d10a144ac
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2024-09-28 23:17:20 +0530

    added ts_ls and telescope select ui

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  {
+  'nvim-telescope/telescope.nvim', tag = '0.1.8',
 -- or                              , branch = '0.1.x',
       dependencies = { 'nvim-lua/plenary.nvim' },
       config = function()
@@ -8,6 +9,22 @@ return {
       vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
       vim.keymap.set('n', '<C-s>', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' }) 
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
       end
-    }
+  },
+  {
+	"nvim-telescope/telescope-ui-select.nvim",
+	config = function()
+	require("telescope").setup {
+	  extensions = {
+		["ui-select"] = {
+		  require("telescope.themes").get_dropdown {
+		  }
+		}
+	  }
+	}
+	require("telescope").load_extension("ui-select")
+	end
+  }
+}
+
