47c582c503687f88aa958145588d1c7095b9a9eb
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2024-09-29 18:32:47 +0530

    refactored the git keybinds

--- lua/plugins/git-config.lua
+++ lua/plugins/git-config.lua
@@ -7,13 +7,13 @@ return {
 		config = function()
 			require("gitsigns").setup()
 			-- Actions
-			vim.keymap.set("n", "<leader>gs", ":Gitsigns stage_hunk<CR>")
-			vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>")
-			vim.keymap.set("n", "<leader>gS", ":Gitsigns stage_buffer<CR>")
-			vim.keymap.set("n", "<leader>gu", ":Gitsigns undo_stage_hunk<CR>")
+			vim.keymap.set("n", "<leader>gs", ":Gitsigns stage_hunk <CR>")
+			vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk <CR>")
+			vim.keymap.set("n", "<leader>gS", ":Gitsigns stage_buffer <CR>")
+			vim.keymap.set("n", "<leader>gu", ":Gitsigns undo_stage_hunk <CR>")
 			vim.keymap.set("n", "<leader>gR",":Gitsigns reset_buffer <CR>")
 			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk <CR>")
-			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>")
+			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame <CR>")
 			vim.keymap.set("n", "<leader>gd", ":Gitsigns diffthis <CR>")
 			vim.keymap.set("n", "<leader>td", ":Gitsigns toggle_deleted <CR>")
 		end,
