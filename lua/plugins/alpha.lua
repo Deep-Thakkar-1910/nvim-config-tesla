933d5322f17116eef29e968bd6f52bf60cfb0957
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2024-10-13 17:26:35 +0530

    replaced nvim-tree with neo-tree

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -21,7 +21,7 @@ return {
 		dashboard.section.buttons.val = {
 			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
 			dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/ <CR>"),
-			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Neotree toggle left<CR>"),
 			dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
 			dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
 			dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,91 +1,98 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	dependencies = "nvim-tree/nvim-web-devicons",
+	"nvim-neo-tree/neo-tree.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	event = "VeryLazy",
+	keys = {
+		{ "<leader>ee", ":Neotree toggle left<CR>", silent = true, desc = "Left File Explorer" },
+		{ "<leader>eo", ":Neotree toggle float<CR>", silent = true, desc = "Float File Explorer" },
+	},
 	config = function()
-		local nvimtree = require("nvim-tree")
-
-		-- recommended settings from nvim-tree documentation
-		vim.g.loaded_netrw = 1
-		vim.g.loaded_netrwPlugin = 1
-
-		nvimtree.setup({
-			view = {
-				width = 35,
-			},
-			-- change folder arrow icons
-			renderer = {
-				indent_markers = {
-					enable = true,
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			popup_border_style = "single",
+			enable_git_status = true,
+			enable_modified_markers = true,
+			enable_diagnostics = true,
+			sort_case_insensitive = true,
+			default_component_configs = {
+				indent = {
+					with_markers = true,
+					with_expanders = true,
 				},
-			},
-			-- disable window_picker for
-			-- explorer to work well with
-			-- window splits
-			actions = {
-				open_file = {
-					window_picker = {
-						enable = false,
+				modified = {
+					symbol = " ",
+					highlight = "NeoTreeModified",
+				},
+				icon = {
+					folder_closed = "",
+					folder_open = "",
+					folder_empty = "",
+					folder_empty_open = "",
+				},
+				git_status = {
+					symbols = {
+						-- Change type
+						added = "",
+						deleted = "",
+						modified = "",
+						renamed = "",
+						-- Status type
+						untracked = "",
+						ignored = "",
+						unstaged = "",
+						staged = "",
+						conflict = "",
 					},
 				},
 			},
-			filters = {
-				custom = { ".DS_Store" },
+			window = {
+				position = "float",
+				width = 35,
 			},
-			git = {
-				ignore = false,
+			filesystem = {
+				use_libuv_file_watcher = true,
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					hide_by_name = {
+						"node_modules",
+					},
+					never_show = {
+						".DS_Store",
+						"thumbs.db",
+					},
+				},
+			},
+			source_selector = {
+				winbar = true,
+				sources = {
+					{ source = "filesystem", display_name = "   Files " },
+					{ source = "buffers", display_name = "   Bufs " },
+					{ source = "git_status", display_name = "   Git " },
+				},
+			},
+			event_handlers = {
+				{
+					event = "neo_tree_window_after_open",
+					handler = function(args)
+						if args.position == "left" or args.position == "right" then
+							vim.cmd("wincmd =")
+						end
+					end,
+				},
+				{
+					event = "neo_tree_window_after_close",
+					handler = function(args)
+						if args.position == "left" or args.position == "right" then
+							vim.cmd("wincmd =")
+						end
+					end,
+				},
 			},
 		})
-
-		-- function to open nvim-tree in a floating window
-		local function open_nvim_tree_float()
-			-- open nvim-tree
-			require("nvim-tree.api").tree.open()
-
-			-- get the buffer ID of the nvim-tree
-			local bufnr = vim.api.nvim_get_current_buf()
-
-			-- Create a floating window for nvim-tree
-			local width = vim.api.nvim_get_option("columns")
-			local height = vim.api.nvim_get_option("lines")
-			local win_width = math.ceil(width * 0.7)
-			local win_height = math.ceil(height * 0.7)
-			local row = math.ceil((height - win_height) / 2)
-			local col = math.ceil((width - win_width) / 2)
-
-			local opts = {
-				relative = "editor",
-				width = win_width,
-				height = win_height,
-				row = row,
-				col = col,
-				style = "minimal",
-				border = "rounded",
-			}
-
-			-- open nvim-tree in the floating window and get window ID
-			local win_id = vim.api.nvim_open_win(bufnr, true, opts)
-
-			-- Map 'q' to close the floating window
-			vim.api.nvim_buf_set_keymap(bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-
-			-- Optional: Automatically focus the floating window when opened
-			vim.api.nvim_set_current_win(win_id)
-		end
-
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
-
-		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-		keymap.set(
-			"n",
-			"<leader>ef",
-			"<cmd>NvimTreeFindFileToggle<CR>",
-			{ desc = "Toggle file explorer on current file" }
-		) -- toggle file explorer on current file
-		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-
-		-- map for floating file explorer
-		keymap.set("n", "<leader>eo", open_nvim_tree_float, { desc = "Open file explorer in a floating window" })
 	end,
 }
